4177aa2971ad7ff4eee61117d7ddc457a2b9451e
Catarina Ferreira <150731154+catarina-das-merces@users.noreply.github.com>
2024-05-08 17:10:44 +0100

    Update productsRouter.ts

--- backend/src/routers/productsRouter.ts
+++ backend/src/routers/productsRouter.ts
@@ -1,15 +1,36 @@
-import {Router} from "express";
+import { Router } from "express";
+import { checkRoles } from "../middlewares/authMiddleware.js";
 import ProductsController from "../controllers/productControllers.js";
 
 const router = Router();
 // Get all products
-router.get("/products", ProductsController.getAllProducts);
+router.get(
+	"/products",
+	checkRoles(["USER", "ADMIN"]),
+	ProductsController.getAllProducts
+);
 // Get product by ID
-router.get("/products/:id", ProductsController.getProductById);
+router.get(
+	"/products/:id",
+	checkRoles(["USER", "ADMIN"]),
+	ProductsController.getProductById
+);
 // Create a new product
-router.post("/products", ProductsController.createProduct);
+router.post(
+	"/products",
+	checkRoles(["ADMIN"]),
+	ProductsController.createProduct
+);
 // Update an existing product
-router.put("/products/:id", ProductsController.updateProduct);
+router.put(
+	"/products/:id",
+	checkRoles(["ADMIN"]),
+	ProductsController.updateProduct
+);
 // Delete an existing product
-router.delete("/products/:id", ProductsController.deleteProduct);
+router.delete(
+	"/products/:id",
+	checkRoles(["ADMIN"]),
+	ProductsController.deleteProduct
+);
 export default router;
